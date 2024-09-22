5b36b7af76b16634669ea2478076a0bb0d1e5bc5
CasMonDavid <dcastro_21@alu.uabcs.mx>
2024-09-22 02:36:43 -0700

    Activdad 12 completada

--- Unidad 3/proyecto-vue/src/App.vue
+++ Unidad 3/proyecto-vue/src/App.vue
@@ -1,71 +1,108 @@
 <script setup>
-  import { ref } from 'vue'
-  let name = ref('David')
+  import { ref, onMounted, onUpdated, onUnmounted } from 'vue'
+  let users = ref([])
+  let name = ref('')
   let username = ref('')
   let email = ref('')
   let phone = ref('')
+  let password = ref('')
   let isLogin = ref(false)
   let isAddNewUser = ref(false)
+  
+  function checkSession(){
+   name.value = localStorage.getItem('SessionName')
+    if (nameSession!=undefined){
+      isLogin=true;
+      console.log("Nombre de la sesion: "+nameSession);
+    }else{
+      console.log("No hay ninguna sesion");
+    }
+  };
 
-  methods: {
-    function onSubmit()
-        {
-	    		console.log(this.email) 
-	    		console.log(this.password)  
-
-	    		fetch('users.json',) 
-	    		.then(res=>res.json())
-	    		.then((response) => { 
-
-				    response.forEach((e) => {
-
-				    	if (e.email==this.email) {
-				    		if(e.password == this.password){
-
-				    			console.log("Login")
-				    			this.isLogin = true
-				    		}
-				    	}
-
-				    });
-
-				    if (!this.isLogin) {
-				    	alert("Verifique su información")
-				    }
-
-
-				 })
-	    		.catch((error) => {
-				    
-	    			alert("Ha ocurrido un error")
-
-				});
-	    }
+  function onSubmit(){
+    fetch('./users.json') 
+    .then(res => res.json())
+    .then((response) => {
+      users = response
+      response.forEach((e) => {
+        if (e.email == email.value){
+          if (e.password == password.value){
+            isLogin.value = true
+            sessionStorage.setItem('SessionName',e.name)
+            name.value = sessionStorage.getItem("SessionName")
+            alert("Bienvenido al sistema "+e.name)
+          }
+        }
+      })
+    }).catch((error) => {
+      console.error('Error durante la recoleción de datos :', error)
+    });
   }
+
+  onMounted(() => {
+    name.value = sessionStorage.getItem("SessionName")
+
+    fetch('./users.json') 
+    .then(res => res.json())
+    .then((response) => {
+      users.value = response
+    }).catch((error) => {
+      console.error('Error durante la recoleción de datos :', error)
+    });
+
+    if (name.value!=undefined){
+      console.log("Sesion abierta de: "+name.value)
+      isLogin.value = true;
+    }
+    console.log("los componentes son montados ")
+  })
 </script>
 
 <template>
   <div class="cuerpo">
-    <div v-if="!isLogin">
+    <form v-if="!isLogin" @submit.prevent="onSubmit">
       <div id="titulo">
         <h1>Iniciar sesión</h1>
       </div>
-      <div id="campos">
-        <div id="campoCorreo">
-          <label for="">Ingresar correo: </label>
-          <input v-model="correo" type="text" placeholder="Correo">
-        </div>
-        <div id="campoPassword">
-          <label for="">Ingresar contraseña: </label>
-          <input v-model="password" type="password" placeholder="Contraseña">
-        </div>
-        <button type="submit">Acceder</button>
-      </div>
-    </div>
+      <fieldset id="campoCorreo">
+        <label for="">Ingresar correo: </label>
+        <input v-model="email" type="text" placeholder="Correo">
+      </fieldset>
+      <fieldset id="campoPassword">
+        <label for="">Ingresar contraseña: </label>
+        <input v-model="password" type="password" placeholder="Contraseña">
+      </fieldset>
+      <button type="submit">Acceder</button>
+    </form>
     <div v-else>
       <h2>Bienvenido {{ name }}</h2>
       <div v-if="!isAddNewUser">
-
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Nombre de usuario</th>
+              <th>Correos</th>
+              <th>Teléfonos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="dato in users">
+              <td>
+                {{ dato.name }}
+              </td>
+              <td>
+                {{ dato.username }}
+              </td>
+              <td>
+                {{ dato.email }}
+              </td>
+              <td>
+                {{ dato.phone }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       <div v-else>
       <form>
@@ -99,6 +136,12 @@
     flex-direction: column;
     align-items: center;
     background-color: burlywood;
+    padding: 30px;
+  }
+  #titulo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   #titulo h1{
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
@@ -119,5 +162,11 @@
   }
   h2{
     margin: 10px 0px;
+  }
+  thead{
+    background-color: lightblue;
+  }
+  tbody{
+    background-color: lightgray;
   }
 </style>
