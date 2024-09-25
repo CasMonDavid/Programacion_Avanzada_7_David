7a6f16b21b31f201cf46bcebaccc56cd802b5a50
CasMonDavid <dcastro_21@alu.uabcs.mx>
2024-09-25 05:50:58 -0700

    Actividad 18 completa

--- Unidad 3/proyecto-vue/src/App.vue
+++ Unidad 3/proyecto-vue/src/App.vue
@@ -1,125 +1,145 @@
 <script setup>
   import { ref, onMounted } from 'vue'
-  let users = ref([])
+  let movies = ref([])
+  let movieDetails = ref([])
   let SessionName = ref('')
-  let id = ref(0)
-  let name = ref('')
   let username = ref('')
-  let email = ref('')
-  let phone = ref('')
   let password = ref('')
+  let points = ref(0.0)
   let isLogin = ref(false)
-  let isAddNewUser = ref(false)
-  let isEditUser = ref(false)
-  
-  function changeIsAddNewUser(){
-    isAddNewUser.value = !isAddNewUser.value;
-  }
-
-  function changeIsEditUser(){
-    isEditUser.value = !isEditUser.value;
-    isAddNewUser.value = !isAddNewUser.value;
-
-    clearParams()
-  }
-
-  function addNewUser(){
-    let newUser = {
-      id: (users.value.length+1),
-      name: name.value,
-      username: username.value,
-      email: email.value,
-      password: "abc123",
-      phone: phone.value
-    }
-    users.value.push(newUser)
-    console.log(users.value)
-    clearParams()
-    changeIsAddNewUser()
-  }
+  let isDetails = ref(false)
 
   function onSubmit(){
-    fetch('./users.json') 
-    .then(res => res.json())
-    .then((response) => {
-      let responseString = JSON.parse(JSON.stringify(response))
-      users.value = responseString
-      response.forEach((e) => {
-        if (e.email == email.value){
-          if (e.password == password.value){
-            isLogin.value = true
-            sessionStorage.setItem('SessionName',e.name)
-            SessionName.value = sessionStorage.getItem("SessionName")
-            alert("Bienvenido al sistema "+e.name)
-            email.value=""
-            password.value=""
-          }
-        }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWQ4MDQzMTU0MWViYTZiYjQ0MjE2MWZhYTJjMDA3ZCIsIm5iZiI6MTcyNzI1MTAyOS42NzEzMjksInN1YiI6IjY2ZjJmNzA0YTgyYjAwNTcwMzI3MDA3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UWXKuIEaETJqJXBtAPRUkQD8mR66-gCu_ILi-4CE6AM'
+      },
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+        request_token: ''
       })
-    }).catch((error) => {
-      console.error('Error durante la recoleción de datos :', error)
-    });
+    };
+
+    fetch('https://api.themoviedb.org/3/authentication/token/validate_with_login', options)
+      .then(response => response.json())
+      .then(response => {
+        let responseString = JSON.parse(JSON.stringify(response))
+        isLogin.value = true
+        SessionName.value = username.value
+        sessionStorage.setItem('SessionName',username.value)
+        console.log(responseString)
+      })
+      .catch(err => console.error(err));
   }
 
-  function deleteUser (id){
-    users.value.splice((id-1),1)
+  function getMovies(){
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWQ4MDQzMTU0MWViYTZiYjQ0MjE2MWZhYTJjMDA3ZCIsIm5iZiI6MTcyNzI1MTAyOS42NzEzMjksInN1YiI6IjY2ZjJmNzA0YTgyYjAwNTcwMzI3MDA3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UWXKuIEaETJqJXBtAPRUkQD8mR66-gCu_ILi-4CE6AM'
+      }
+    };
+
+    fetch('https://api.themoviedb.org/3/discover/movie?language=es-MX&page=5&sort_by=popularity.desc', options)
+      .then(response => response.json())
+      .then(response => {
+        let responseString = JSON.parse(JSON.stringify(response.results))
+        movies.value = responseString
+        console.log(movies.value)
+      })
+      .catch(err => console.error(err));
   }
 
-  function clearParams(){
-    id=0
-    name.value=""
-    username.value =""
-    email.value=""
-    phone.value=""
-    password=""
+  function goDetailsMovie(idMovie){
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWQ4MDQzMTU0MWViYTZiYjQ0MjE2MWZhYTJjMDA3ZCIsIm5iZiI6MTcyNzI1Mzc0NS4zMDc3NTgsInN1YiI6IjY2ZjJmNzA0YTgyYjAwNTcwMzI3MDA3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aLFNRAfX0zEb8gxVh2_XA3tyjXYT5l6pnkiqydvHazM'
+      }
+    };
+
+    fetch(`https://api.themoviedb.org/3/movie/${idMovie}?language=es-MX`, options)
+      .then(response => response.json())
+      .then(response => {
+        let responseString = JSON.parse(JSON.stringify(response))
+        movieDetails = responseString
+        console.log(movieDetails)
+        isDetails.value = !isDetails.value
+      })
+      .catch(err => console.error(err));
+
   }
 
-  function editUser (idUser){
-    changeIsEditUser()
-    
-    const user = users.value.find((element) => element.id == idUser)
-    id = idUser
-    name.value = user.name
-    username.value = user.username
-    email.value = user.email
-    password = user.password
-    phone.value = user.phone
-  }
+  function darValoracion(){
+    let valoracion = parseFloat(points.value)
+    console.log(valoracion)
+    if (valoracion>0){
+      const options = {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json;charset=utf-8',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWQ4MDQzMTU0MWViYTZiYjQ0MjE2MWZhYTJjMDA3ZCIsIm5iZiI6MTcyNzI1Mzc0NS4zMDc3NTgsInN1YiI6IjY2ZjJmNzA0YTgyYjAwNTcwMzI3MDA3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aLFNRAfX0zEb8gxVh2_XA3tyjXYT5l6pnkiqydvHazM'
+        },
+        body: `{"value":${valoracion}}`
+      };
 
-  function editUserConfirm(){
-    console.log("Id: "+id)
-    console.log(users.value)
-    let confirmDataUser = {
-      id: id,
-      name: name.value,
-      username: username.value,
-      email: email.value,
-      password: password,
-      phone: phone.value
+      fetch(`https://api.themoviedb.org/3/movie/${movieDetails.id}/rating`, options)
+        .then(response => response.json())
+        .then(response => {
+          console.log(response)
+          alert("Gracias por votar")
+        })
+        .catch(err => console.error(err));
+    }else{
+      alert("La puntuaciones tienen que ser minimo de 1")
     }
-    users.value.splice((id-1),1,confirmDataUser)
-    console.log("Despues de de ejecutar: ")
-    console.log(users.value)
+  }
 
-    changeIsEditUser()
+  function deleteValoracion(){
+    const options = {
+      method: 'DELETE',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWQ4MDQzMTU0MWViYTZiYjQ0MjE2MWZhYTJjMDA3ZCIsIm5iZiI6MTcyNzI1Mzc0NS4zMDc3NTgsInN1YiI6IjY2ZjJmNzA0YTgyYjAwNTcwMzI3MDA3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aLFNRAfX0zEb8gxVh2_XA3tyjXYT5l6pnkiqydvHazM'
+      }
+    };
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieDetails.id}/rating`, options)
+      .then(response => response.json())
+      .then(response => {
+        let responseString = JSON.parse(JSON.stringify(response))
+        console.log(responseString)
+        alert("Se elimino tu puntuación")
+        points.value = 0
+      })
+      .catch(err => console.error(err));
+  }
+
+  function volverMenu(){
+    isDetails.value = false
   }
 
   onMounted(() => {
     SessionName.value = sessionStorage.getItem("SessionName")
 
-    fetch('./users.json') 
-    .then(res => res.json())
-    .then((response) => {
-      let responseString = JSON.parse(JSON.stringify(response))
-      users.value = responseString
-    }).catch((error) => {
-      console.error('Error durante la recoleción de datos :', error)
-    });
-
     if (SessionName.value!=undefined){
       console.log("Sesion abierta de: "+SessionName.value)
       isLogin.value = true;
     }
+
+    
+
+    getMovies()
+
     console.log("los componentes son montados ")
   })
 </script>
@@ -131,99 +151,52 @@
         <h1>Iniciar sesión</h1>
       </div>
       <fieldset id="campoCorreo">
-        <legend for="">Ingresar correo </legend>
-        <input v-model="email" type="text" placeholder="Correo">
+        <legend for="">Ingresar nombre de usuario</legend>
+        <input v-model="username" type="text" placeholder="Nombre de usuario">
       </fieldset>
       <fieldset id="campoPassword">
-        <legend for="">Ingresar contraseña </legend>
+        <legend for="">Ingresar contraseña</legend>
         <input v-model="password" type="password" placeholder="Contraseña">
       </fieldset>
       <button type="submit">Acceder</button>
     </form>
     <div v-else>
       <h2>Bienvenido {{ SessionName }}</h2>
-      <div v-if="!isAddNewUser">
-        <button @click="changeIsAddNewUser">Agregar un usuario</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Identificador</th>
-              <th>Nombre</th>
-              <th>Nombre de usuario</th>
-              <th>Correos</th>
-              <th>Teléfonos</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="dato in users">
-              <td>
-                {{ dato.id }}
-              </td>
-              <td>
-                {{ dato.name }}
-              </td>
-              <td>
-                {{ dato.username }}
-              </td>
-              <td>
-                {{ dato.email }}
-              </td>
-              <td>
-                {{ dato.phone }}
-              </td>
-              <td>
-                <button @click="editUser(dato.id)" >Editar</button>
-                <button @click="deleteUser(dato.id)" >Eliminar</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div v-if="!isDetails">
+      <h1>PELICULAS:</h1>
+      <div class="row"> <!-- Poner el for aqui -->
+        
+        <div v-for="peli in movies" class="card column">
+          <img :src="`https://image.tmdb.org/t/p/w200${peli.poster_path}`" alt="Avatar" style="width:100%">
+          <div class="container">
+            <h4><b>Título original: {{ peli.original_title }}</b></h4>
+            <h4><b>Título: {{ peli.title }}</b></h4>
+            <p>Fecha: {{ peli.release_date }}</p>
+            <button @click="goDetailsMovie(peli.id)" class="btnDetalles">Detalles</button>
+          </div>
+        </div>
+
       </div>
-      <div v-else-if="isEditUser">
-        <form @submit.prevent="editUserConfirm">
-          <fieldset>
-            <legend>Editar nombre </legend>
-            <input type="text" id="name" v-model="name" placeholder="Nombre" required>
-          </fieldset>
-          <fieldset>
-            <legend>Editar nombre de usuario </legend>
-            <input type="text" id="username" v-model="username" placeholder="Nombre de usuario" required>
-          </fieldset>
-          <fieldset>
-            <legend>Editar correo </legend>
-            <input type="text" id="email" v-model="email" placeholder="Correo" required>
-          </fieldset>
-          <fieldset>
-            <legend>Editar número telefónico </legend>
-            <input type="text" id="phone" v-model="phone" placeholder="Teléfono" required>
-          </fieldset>
-          <button type="submit">Confirmar cambios</button>
-          <button @click="changeIsEditUser">Cancelar</button>
-        </form>
       </div>
-      <div v-else>
-        <form @submit.prevent="addNewUser">
-          <fieldset>
-            <legend>Agregar un nombre </legend>
-            <input type="text" id="name" v-model="name" placeholder="Nombre" required>
-          </fieldset>
-          <fieldset>
-            <legend>Agregar un nombre de usuario </legend>
-            <input type="text" id="username" v-model="username" placeholder="Nombre de usuario" required>
-          </fieldset>
-          <fieldset>
-            <legend>Agregar un correo </legend>
-            <input type="text" id="email" v-model="email" placeholder="Correo" required>
-          </fieldset>
-          <fieldset>
-            <legend>Agregar un número telefónico </legend>
-            <input type="text" id="phone" v-model="phone" placeholder="Teléfono" required>
-          </fieldset>
-          <button type="submit">Agregar</button>
-          <button @click="changeIsAddNewUser">Cancelar</button>
-        </form>
+      <div v-else class="cuerpoDetalles">
+        <div id="contenedorDetalles">
+          <div>
+            <h1>{{ movieDetails.title }}</h1>
+            <img :src="`https://image.tmdb.org/t/p/w200${movieDetails.poster_path}`" alt="">
+          </div>
+          <div id="detallesInformacion">
+            <h3>Fecha: {{ movieDetails.release_date }}</h3>
+            <h3>Idioma original: {{ movieDetails.original_language }}</h3>
+            <h3>Estado: {{ movieDetails.status }}</h3>
+            <h3>¡Dar valoración!: {{ points }}</h3>
+            <input v-model="points" type="range" min="1" max="10">
+            <button @click="darValoracion" class="btnDetalles" >Confirmar</button>
+            <button @click="deleteValoracion" class="btnDetalles" >Eliminar</button>
+            <button @click="volverMenu" class="btnDetalles" >Volver</button>
+          </div>
+        </div>
       </div>
+      
     </div>
     
   </div>
@@ -246,13 +219,6 @@
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     font-weight: bold;
   }
-  #campos{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 30px;
-    padding: 20px;
-  }
   #campoCorreo{
     padding-bottom: 10px;
   }
@@ -262,10 +228,60 @@
   h2{
     margin: 10px 0px;
   }
-  thead{
-    background-color: lightblue;
+
+  .column {
+    float: left;
+    width: 23%;
+    padding: 0 10px;
+    height: 550px;
   }
-  tbody{
-    background-color: lightgray;
+  .row {
+    margin: 0 -5px;
+  }
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: 16px;
+    text-align: center;
+    background-color: #f1f1f1;
+    margin: 5px 5px;
+  }
+  .card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+  .container {
+    padding: 2px 16px;
+  }
+
+  .btnDetalles{
+    font-size: large;
+    margin: 2px;
+  }
+
+  .cuerpoDetalles{
+    background-color: aquamarine;
+    width: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+  #contenedorDetalles{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin: 20px;
+    padding: 10px;
+  }
+  #detallesInformacion{
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    padding: 0px 20px;
+    width: 100%;
   }
 </style>
