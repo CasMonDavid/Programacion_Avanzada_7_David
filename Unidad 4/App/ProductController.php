25387120a88ad7f14e5cfeb77e06aad1e8a472b0
CasMonDavid <90111855+CasMonDavid@users.noreply.github.com>
2024-10-16 10:04:30 -0700

    New docs

--- Unidad 4/App/ProductController.php
+++ Unidad 4/App/ProductController.php
@@ -1,5 +1,32 @@
 <?php
 
+    class function productController(){
+        
+        function getAllProducts() {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Bearer 273|mxuzNTNtOd6i52UTtphPPRNmpJg6ioS3j5oQgswJ'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
     
+        }
+
+    }
 
 ?>
